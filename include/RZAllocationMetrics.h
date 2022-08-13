e966bc8eea34dd424de3e6988ef30e2f8b6d83ec
Pikachuxxxx <Phani.s2909@icloud.com>
2022-08-13 08:01:11 +0530

    filled allocation tracking structs to use a clusteer-cell like allocations

--- include/RZAllocationMetrics.h
+++ include/RZAllocationMetrics.h
@@ -4,43 +4,54 @@
 
 #include "RZMemoryTags.h"
 
-struct RZMemAllocationInfo
+/**
+ * Allocations will be divided into clusters depending on the category of Allocations such as GRAPHICS, SCENE, PHYSICS, SHARED_THREAD pools etc
+ * Each cluster will have cells that contain same types for fixed aligned Allocations ex. a GRAPHICS cluster will have cells for VertexBuffer, IndexBuffer etc.
+ */
+
+struct RZMemAllocInfo
 {
     const char* name;
     uint32_t    line;
+    const char* file;
     MemoryTag   tag;
     void*       address;
     size_t      size;
 };
 
-struct RZMemClusterAllocationInfo : public RZMemAllocationInfo
+struct RZMemClusterAllocInfo : public RZMemAllocInfo
 {
     uint32_t clusterID;
     uint32_t clusterSize;
 };
 
-struct RZMemView
+struct RZMemCellAllocInfo : public RZMemClusterAllocInfo
 {
-
+    uint32_t elementSize;
+    uint32_t elementsCount;
+    uint32_t cellID;
+    uint8_t  cellAlignment;
 };
 
-struct RZMemAllocationSnapshot
+struct RZMemView
 {
-    void AddAllocation(const RZMemAllocationInfo& allocationInfo);
-    void AddAllocationCluster(const RZMemClusterAllocationInfo& allocationClusterInfo);
+};
 
-    bool CaptureAllocations(RZMemView& view);
-    // This is a debug method to print total allocations
+struct RZMemAllocSnapshot
+{
+    void AddAlloc(const RZMemAllocInfo& AllocInfo);
+    void AddAllocCluster(const RZMemClusterAllocInfo& AllocClusterInfo);
+
+    bool CaptureAllocs(RZMemView& view);
+    // This is a debug method to print total Allocations
     void DisplayView(const RZMemView& view);
 };
 
 struct RZMemInfoElement
 {
-
 };
 
-class RZMemoAllocationInfoIterator
+class RZMemoAllocInfoIterator
 {
 public:
-
 };
--- include/RZMemoryTags.h
+++ include/RZMemoryTags.h
@@ -1,7 +1,15 @@
 #pragma once
 
+// TODO: Separate common tags and Thread tags to enforce more strict rules
+/**
+ * Used to tag memory to create cluster pools
+ * 
+ * Note:- Multiple types can be OR-ed together for shared allocation however it's recommended
+ * only to OR common tags and Threads for optimal usage
+ */
 enum class MemoryTag
 {
+    // Common Tags
     GRAPHICS, 
     CORE, 
     APPLICATION, 
@@ -11,6 +19,7 @@ enum class MemoryTag
     COMMON, 
     MISC, 
     UNKNOWN, 
+    // VERY IMPORTANT TAGS !!!
     THREAD_GLOBAL,
     THREAD_SHARED
 };
