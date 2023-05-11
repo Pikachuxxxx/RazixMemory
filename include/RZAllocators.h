01c243b08ed79e2430c0303d640fc704bc615372
Pikachuxxxx <Phani.s2909@icloud.com>
2023-05-12 01:09:48 +0530

    Update include file

---
+++ include/RZAllocators.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "Allocators/IRZAllocator.h"
+#include "Allocators/RZHeapAllocator.h"
+#include "Allocators/RZLinearAllocator.h"
+#include "Allocators/RZStackAllocator.h"
+#include "Allocators/RZTwoSidedAllocator.h"
--- include/RZMemory.h
+++ include/RZMemory.h
@@ -2,14 +2,9 @@
 
 // Contains all the files for custom memory
 
+#include "RZAllocators.h"
 #include "RZMemoryConfig.h"
 #include "RZMemoryFunctions.h"
 #include "RZMemoryPools.h"
 #include "RZMemoryRoot.h"
 #include "RZMemoryTags.h"
-
-#include "Allocators/IRZAllocator.h"
-#include "Allocators/RZHeapAllocator.h"
-#include "Allocators/RZLinearAllocator.h"
-#include "Allocators/RZStackAllocator.h"
-#include "Allocators/RZTwoSidedAllocator.h"
